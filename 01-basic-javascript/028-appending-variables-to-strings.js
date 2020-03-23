93cfcdcd0f0b1ccc4368c8270149f51f904afb3c
Rafael Martins <rafaeldcmartins@gmail.com>
2020-03-23 12:38:33 -0300

    add solutions from 24 to 33

---
+++ 01-basic-javascript/028-appending-variables-to-strings.js
@@ -0,0 +1,10 @@
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "cool!";
+var myStr = "Learning to code is ";
+console.log(myStr += someAdjective);
