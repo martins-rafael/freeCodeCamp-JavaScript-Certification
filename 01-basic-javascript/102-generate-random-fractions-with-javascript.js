002e0c0bc8f2689552b16fe65c3440164a27600e
Rafael Martins <rafaeldcmartins@gmail.com>
2020-04-11 22:32:27 -0300

    added 96 to 105

---
+++ 01-basic-javascript/102-generate-random-fractions-with-javascript.js
@@ -0,0 +1,10 @@
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random(); // generates a random decimal number between 0 (inclusive) and not quite up to 1 (exclusive).
+
+  // Only change code above this line
+}
+
+console.log(randomFraction())
---
+++ 01-basic-javascript/103-generate-random-whole-numbers-with-javascript.js
@@ -0,0 +1,11 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line
+
+  return Math.floor(Math.random() * 10);
+}
+
+var zeroNine = randomWholeNum;
+console.log(zeroNine());
