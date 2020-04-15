1059875686d44885bfa28b3568c23dea998083cf
Rafael Martins <rafaeldcmartins@gmail.com>
2020-04-14 22:43:40 -0300

    added 11 to 15

---
+++ 02-es6/11-use-destructuring-assignment-to-extract-values-from-objects.js
@@ -0,0 +1,12 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+console.log(today, tomorrow);
+
+// Only change code above this line
