be26db07caf4c8f9b525370a44f73d93cf6482e3
Rafael Martins <rafaeldcmartins@gmail.com>
2020-04-15 21:38:15 -0300

    added challenges from 16 to 20

---
+++ 02-es6/18-write-concise-object-literal-declarations-using-object-property-shorthand.js
@@ -0,0 +1,13 @@
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // Only change code above this line
+};
+
+const rafael = createPerson('Rafael', 27, 'Male');
+console.log(rafael);
