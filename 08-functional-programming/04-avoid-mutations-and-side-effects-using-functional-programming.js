79c12f9ae8c5593d6da4c783631a7d5a2971dbea
Rafael Martins <rafaeldcmartins@gmail.com>
2020-06-04 21:25:35 -0300

    add challenges

---
+++ 08-functional-programming/04-avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,10 @@
+// The global variable
+var fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+  let tempValue = fixedValue;
+  return tempValue += 1;
+
+  // Only change code above this line
+}
---
+++ 08-functional-programming/05-pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,9 @@
+// The global variable
+var fixedValue = 4;
+
+// Only change code below this line
+function incrementer(addOne) {
+  return addOne += 1;
+
+  // Only change code above this line
+}
