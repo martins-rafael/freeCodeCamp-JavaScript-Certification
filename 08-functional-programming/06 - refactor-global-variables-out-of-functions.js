163fe7f749f45e4a3f73128cfc3f5105ebdd0476
Rafael Martins <rafaeldcmartins@gmail.com>
2020-06-07 20:57:05 -0300

    add challenges 06 to 10

---
+++ 08-functional-programming/06 - refactor-global-variables-out-of-functions.js
@@ -0,0 +1,30 @@
+// The global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(bookList, bookName) {
+  let newList = [...bookList];
+  newList.push(bookName);
+  return newList;
+
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookList, bookName) {
+  let newList = [...bookList];
+  let book_index = newList.indexOf(bookName);
+  if (book_index >= 0) {
+
+    newList.splice(book_index, 1);
+    return newList;
+
+    // Change code above this line
+  }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
